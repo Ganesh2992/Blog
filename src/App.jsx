235d711d42c1735c50f95374c497a81f7885c3c9
Ganesh Mangalam Sinha <ganeshmangalamsinha@gmail.com>
2026-04-16 03:59:30 +0530

    Added the main file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+    <h1 className='text-10xl bg-black flex items-center justify-center'>Project is Going On</h1>
+    </>
+  )
+}
+
+export default App
