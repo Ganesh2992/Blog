f231f63b418c8255790d4e711ea0161b29647621
Ganesh Mangalam Sinha <ganeshmangalamsinha@gmail.com>
2026-05-02 07:33:06 +0530

    Add More Import

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,24 @@ import Footer from "./Footer/Footer.jsx";
 import Container from "./Container/Container.jsx";
 import Logo from './Logo.jsx';
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import PostForm from "./post-form/PostForm.jsx"
+import PostCard from "./PostCard.jsx"
+import AuthLayout from "./AuthLayout.jsx"
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+
+    PostForm,
+    PostCard,
+    AuthLayout
 }
